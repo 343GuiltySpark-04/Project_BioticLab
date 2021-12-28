a0bdd95a04835482c34e264e580908971f0914ea
Tristan Adams <killerdragonxs@gmail.com>
2021-12-28 18:20:14 -0500

    modified:   main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+class block{
 
+    constructor(index, timestamp, data, previousHash = ''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = '';
+
+    }
+
+    
+
+}
