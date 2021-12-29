ed0e75e0b0b133ea20422fbbd7aad7c7c9821b87
Tristan Adams <killerdragonxs@gmail.com>
2021-12-29 12:26:52 -0500

    modified:   main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const SHA256 = require("crypto-js/sha256");
 
-class block {
+class Block {
 
     constructor(index, timestamp, data, previousHash = '') {
         this.index = index;
@@ -36,8 +36,26 @@ class Blockchain {
 
     createGenesisBlock() {
 
-        return new block(0, "12/29/2021", "Genesis Block", "203042caf34a");
+        return new Block(0, "01/01/2021", "Genesis Block", "203042caf34a");
 
     }
 
+    getLatestBlock() {
+
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+
 }
+
+let Project_Bioticlab = new Blockchain();
+Project_Bioticlab.addBlock(new Block(1, "12/29/2021", { amount: 4 }));
+Project_Bioticlab.addBlock(new Block(2, "12/30/2021", { amount: 5 }));
+
+console.log(JSON.stringify(Project_Bioticlab, null, 4));
