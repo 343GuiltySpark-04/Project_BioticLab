938271e2922e77f69083887f7f9435222b989a42
Tristan Adams <killerdragonxs@gmail.com>
2021-12-30 09:01:25 -0500

    modified:   main.js

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 const {Blockchain, Transaction} = require('./blockchain');
+const EC = require("elliptic").ec;
+const ec = new EC("secp256k1");
+
+const myKey = ec.keyFromPrivate('b11af8da4f6c6912cf12007d7f30f85976c131f031d1cd69142c62e82ac422bf');
+const myWalletAddress = myKey.getPublic('hex');
+const targetAddress = '';
+const amountToSend = 0;
 
 let Project_Bioticlab = new Blockchain();
-Project_Bioticlab.createTransaction(new Transaction('address1', 'address2', 100));
-Project_Bioticlab.createTransaction(new Transaction('address2', 'address1', 50));
+
+
+const txl = new Transaction(myWalletAddress, 'targetAddress', amountToSend);
+txl.signTransaction(myKey);
+Project_Bioticlab.addTransaction(txl);
+
+
+
 
 console.log('\n Starting the miner....');
-Project_Bioticlab.minePendingTransaction('my-address');
-console.log('\n Balance of account is: ', Project_Bioticlab.getBalanceOfAddress('my-address'));
+Project_Bioticlab.minePendingTransaction(myWalletAddress);
+//console.log('\n Balance of account is: ', Project_Bioticlab.getBalanceOfAddress(myWalletAddress));
