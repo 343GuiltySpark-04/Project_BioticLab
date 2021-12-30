408e98fd3da32ef8d1439360e22d7db39e52b86e
Tristan Adams <killerdragonxs@gmail.com>
2021-12-29 19:30:14 -0500

    new file:   blockchain.js 	modified:   main.js

--- main.js
+++ main.js
@@ -2,136 +2,136 @@ const SHA256 = require("crypto-js/sha256");
 var blocksMined = 0;
 var currentBlockNumber = 2;
 
-class Block {
-
-    constructor(index, timestamp, data, previousHash = '') {
-        this.index = index;
-        this.timestamp = timestamp;
-        this.data = data;
-        this.previousHash = previousHash;
-        this.hash = this.calculateHash();
-        this.nonce = 0;
-
-    }
-
-    calculateHash() {
-
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
-
-    }
-
-    mineBlock(difficulty) {
-
-
-
-        console.log("Mining Block: " + currentBlockNumber + "....");
-
-        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
-
-            this.nonce++;
-            this.hash = this.calculateHash();
-
-        }
-
-
-        blocksMined++;
-        currentBlockNumber++;
-        let inSync = this.blockNumberCheck();
-        console.log("Block mined: " + this.hash);
-        console.log("Number of blocks mined: " + blocksMined);
-        //console.log("Is blockchain valid?: " + Blockchain.isChainValid());
-
-        if (inSync == false) {
-
-            currentBlockNumber = blocksMined
-            currentBlockNumber++;
-
-        }
-
-
-
-    }
-
-    blockNumberCheck() {
-
-        let x = blocksMined;
-
-        if (currentBlockNumber !== x + 1) {
-
-            return false;
-
-        }
-
-        return true;
-
-
-    }
-
-
-
+class Transaction {
+  constructor(fromAddress, toAddress, amount) {
+    this.fromAddress = fromAddress;
+    this.toAddress = toAddress;
+    this.amount = amount;
+  }
 }
 
+class Block {
+  constructor(timestamp, transactions, previousHash = "") {
+    this.timestamp = timestamp;
+    this.transactions = transactions;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+    this.nonce = 0;
+  }
+
+  calculateHash() {
+    return SHA256(
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.transactions) +
+        this.nonce
+    ).toString();
+  }
+
+  mineBlock(difficulty) {
+    console.log("Mining Block: " + currentBlockNumber + "....");
+
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+  }
+}
 
 class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock];
+    this.difficulty = 2;
+    this.pendingTransactions = [];
+    this.miningReward = 100;
+  }
 
-    constructor() {
+  blockNumberCheck() {
+    let x = blocksMined;
 
-        this.chain = [this.createGenesisBlock];
-        this.difficulty = 4;
-
-
-
+    if (currentBlockNumber !== x + 1) {
+      return false;
     }
 
-    createGenesisBlock() {
+    return true;
+  }
 
-        return new Block(0, "01/01/2021", "Genesis Block", "203042caf34a");
+  createGenesisBlock() {
+    return new Block("01/01/2021", "Genesis Block", "203042caf34a");
+  }
 
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  minePendingTransaction(miningRewardAddress) {
+    let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock.hash);
+    block.mineBlock(this.difficulty);
+
+    blocksMined++;
+    currentBlockNumber++;
+    let inSync = this.blockNumberCheck();
+    console.log("Block mined successfuly!");
+    console.log("Number of blocks mined: " + blocksMined);
+    this.chain.push(block);
+    console.log("Is blockchain valid?: " + this.isChainValid());
+
+    if (inSync == false) {
+      currentBlockNumber = blocksMined;
+      currentBlockNumber++;
     }
 
-    getLatestBlock() {
+    this.pendingTransactions = [
+      new Transaction(null, miningRewardAddress, this.miningReward),
+    ];
+  }
 
-        return this.chain[this.chain.length - 1];
-    }
+  createTransaction(transaction) {
+    this.pendingTransactions.push(transaction);
+  }
 
-    addBlock(newBlock) {
+  getBalanceOfAddress(address) {
+    let balance = 0;
 
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
-        console.log("Is blockchain valid?: " + this.isChainValid());
-    }
-
-    isChainValid() {
-        for (let i = 1; i < this.chain.length; i++) {
-
-            const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i - 1];
-
-            if (currentBlock.hash !== currentBlock.calculateHash()) {
-
-                return false;
-
-            }
-
-            if (currentBlock.previousHash !== previousBlock.hash) {
-
-                return false;
-
-            }
-
+    for (const block of this.chain) {
+      for (const trans of block.transactions) {
+        if (trans.fromAddress === address) {
+          balance -= trans.amount;
         }
 
-        return true;
-
+        if (trans.toAddress === address) {
+          balance += trans.amount;
+        }
+      }
     }
 
+    return balance;
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
 
 let Project_Bioticlab = new Blockchain();
-Project_Bioticlab.addBlock(new Block(1, "12/29/2021", { amount: 4 }));
-Project_Bioticlab.addBlock(new Block(2, "12/30/2021", { amount: 5 }));
+Project_Bioticlab.createTransaction(new Transaction('address1', 'address2', 100));
+Project_Bioticlab.createTransaction(new Transaction('address2', 'address1', 50));
 
-//console.log("Is blockchain vaild?: " + Project_Bioticlab.isChainValid());
-
-//console.log(JSON.stringify(Project_Bioticlab, null, 4));
+console.log('\n Starting the miner....');
+Project_Bioticlab.minePendingTransaction('my-address');
+console.log('\n Balance of account is: ', Project_Bioticlab.getBalanceOfAddress('my-address'));
